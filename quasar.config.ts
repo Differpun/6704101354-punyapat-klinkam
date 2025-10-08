e7bb58242c4b8da1a3f23635e0a7a46c90ebe33f
unknown <differpun2549@gmail.com>
2025-10-08 10:24:44 +0700

    CI/CD

--- quasar.config.ts
+++ quasar.config.ts
@@ -37,6 +37,7 @@ export default defineConfig((/* ctx */) => {
     // Full list of options: https://v2.quasar.dev/quasar-cli-vite/quasar-config-file#build
     build: {
       target: {
+        publicPath: '/6704101354-punyapat-klinkam/',
         browser: [ 'es2022', 'firefox115', 'chrome115', 'safari14' ],
         node: 'node20'
       },
