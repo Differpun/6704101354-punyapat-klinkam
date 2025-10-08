d79a2798e9bfc9cf4cb9ac7233564b307bb79af2
unknown <differpun2549@gmail.com>
2025-10-08 09:35:14 +0700

    Enabled Notify plugin in quasar.config.js

--- quasar.config.ts
+++ quasar.config.ts
@@ -65,7 +65,7 @@ export default defineConfig((/* ctx */) => {
 
       // extendViteConf (viteConf) {},
       // viteVuePluginOptions: {},
-      
+
       // vitePlugins: [
       //   [ 'package-name', { ..pluginOptions.. }, { server: true, client: true } ]
       // ]
@@ -92,7 +92,9 @@ export default defineConfig((/* ctx */) => {
       // directives: [],
 
       // Quasar plugins
-      plugins: []
+      plugins: [
+        'Notify'
+      ]
     },
 
     // animations: 'all', // --- includes all animations
